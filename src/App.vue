1e8b2ddc3b50d8e4a093d4d71bdf91fe1e1c920a
Long Zer <gialongminecraft@gmail.com>
2023-04-27 22:06:30 +0700

    fix spinning

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
-  <div id="app">
+  <!-- <div>
+    <h1>My App</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
+  </div><button></button> -->
+  <div id="">
     <ServerStatus />
   </div>
 </template>
