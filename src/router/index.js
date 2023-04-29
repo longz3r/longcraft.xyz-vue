89e2bb27f7e5a34c82b6411100e0c2c296699c0d
Long Zer <gialongminecraft@gmail.com>
2023-04-29 11:16:45 +0700

    not foudn page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import modpackView from "../views/Modpack.vue"
+import NotFound from "../views/NotFound.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
     {
       path: '/',
       name: 'home',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pt-20 font-minecraftia text-6xl text-red-700">
+    404: NOT FOUND
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
