23dee6b4c05b6fcf18d8b09378abc96ceaaa1095
Long Zer <gialongminecraft@gmail.com>
2023-07-11 11:34:59 +0700

    Update serverStatus.vue

--- src/components/serverStatus.vue
+++ src/components/serverStatus.vue
@@ -38,7 +38,7 @@
               Add server (Bedrock)
             </span>
           </a>
-          <!-- <a href="minecraft://?joinServer=bedrock.longcraft.net:25378" rel="norefferer" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+          <!-- <a href="minecraft://?joinServer=bedrock.longcraft.net:8225" rel="norefferer" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 inline-flex items-center">
               <img src="../assets/plus.svg" width="30" class="pr-2">
               Quick connect (Bedrock)
