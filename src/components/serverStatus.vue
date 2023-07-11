8c9b4b5214a64d243234000c295097bbaaf2a4f7
Long Zer <gialongminecraft@gmail.com>
2023-07-11 22:47:10 +0700

    Update serverStatus.vue

--- src/components/serverStatus.vue
+++ src/components/serverStatus.vue
@@ -30,7 +30,7 @@
         </div>
         <div>Java IP: longcraft.xyz</div>
         <div>Bedrock IP: bedrock.longcraft.xyz</div>
-        <div class="pb-5">Bedrock port: 8225 (Unavailable)</div>
+        <div class="pb-5">Bedrock port: 19134</div>
         <!-- <div v-if="status.online">Max Players: {{ status.players.max }}</div> -->
           <a :href="'minecraft://?addExternalServer=LongCraft%20SMP|bedrock.longcraft.xyz:' + port" rel="norefferer" class="relative inline-flex items-center justify-center p-0.5 mb-5 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 inline-flex items-center">
@@ -65,7 +65,7 @@ export default {
       loading: true,
       error: null,
       status: null,
-      port: 8225
+      port: 19134
     };
   },
   async created() {
