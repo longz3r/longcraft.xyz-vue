f9d3d47e4a9a97da142241cdadfb2bf5f9ba151f
Long Zer <gialongminecraft@gmail.com>
2023-07-19 20:06:17 +0700

    open server map webpage in current tab

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         Discord
       </span>
     </a>
-    <a href="http://map.longcraft.xyz:8225" target="_blank"  class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+    <a href="http://map.longcraft.xyz:8225" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
       <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 inline-flex items-center">
       <img src="./assets/mcMap.webp" width="40" class="pr-2">
         Server map
